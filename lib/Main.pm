3d355fd1d1f7294146b7c4ee270720daf4ad7d8e
Andrey Oboyshikov <andrey.oboyshikov@alpari.org>
2013-03-19 18:44:59 +0400

    Рефакторинг

--- lib/Main.pm
+++ lib/Main.pm
@@ -5,22 +5,24 @@ use Logger::Logger;
 use Data::Dumper;
 use base 'Exporter';
 
-our @EXPORT=qw($config $sysconfig);
-
-$debug_file = "/var/log/netclient.log";
-$logger = new Logger::Logger ( $debug_file, 1 ) or die "Can't create object: Logger::Logger::Error";
+our @EXPORT=qw($config $sysconfig $logger $debug_file);
 
 if ( ! -f "/etc/netclient/logs.yml" or ! -f "/etc/netclient/config.yml" ) {
-	print STDERR "Can't load configuration files!";
-	$logger->debug_message("Can't load configuration files!");
-	exit 1;
+        print STDERR "Can't load configuration files!";
+        $logger->debug_message("Can't load configuration files!");
+        exit 1;
 }
 
+#Грузим конфиг с данными по логам для передачи
 our $config = YAML::Tiny->read("/etc/netclient/logs.yml");
+#Грузим основной конфиг
 our $sysconfig = YAML::Tiny->read("/etc/netclient/config.yml");
 
+#debug log
+$debug_file = $sysconfig->[0]->{'log_file'};
+$logger = new Logger::Logger ( $debug_file, 1 ) or die "Can't create object: Logger::Logger::Error";
 
-
+#TODO
 sub start_proc
 {
 
@@ -65,5 +67,31 @@ return @log_data_to_send;
 
 }
 
-1;
+#function lor tail logs in threads
+sub send_read_log
+{
+my $log_data1 = shift;
 
+($name, $local_path, $server, $remote_path) = split(/::/, $log_data1);
+
+$socket = new IO::Socket::INET (
+PeerHost => $server,
+PeerPort => '9999',
+Proto => 'udp',
+        ) or die "ERROR in Socket Creation : $!\n";
+
+
+my $file=File::Tail->new(name=>"$local_path/$name", maxinterval=>0.1);
+
+        while (defined($line=$file->read)) {
+                $send_text = $name . ":::" . $remote_path . ":::" . $line;
+                $socket->send($send_text);
+                if ( $sysconfig->[0]->{'debug'} eq 1 ) {
+                        $logger->debug_message("Send data: $line");
+                }
+        }
+
+}
+
+#perl...
+1;
